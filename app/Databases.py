678065d03326a1a95f80c80b443e51c2ca088cc0
Mathieu Akoubia <mathieuakoubia@gmail.com>
2024-08-02 18:20:19 +0200

    Update database configuration

--- app/Databases.py
+++ app/Databases.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'mysql+pymysql://root:@host.docker.internal:3306/apimspr')
-
+DATABASE_URL = os.getenv('mysql://q3zvoeyw6iap3dht:xzenl7o0kv0q62gj@enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/n2cduysx6ai148vc')
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
